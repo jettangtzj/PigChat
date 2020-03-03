b70e4d43941330299fb65bb795fce1a4b5af8269
jettangtzj <jet.tang2009@gmail.com>
2020-03-03 14:37:37 +0800

    add database sql file

---
+++ db/zhuxin-dev.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE  IF NOT EXISTS `zhuxin-dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `zhuxin-dev`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: zhuxin-dev
+-- ------------------------------------------------------
+-- Server version	5.7.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chat_msg`
+--
+
+DROP TABLE IF EXISTS `chat_msg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat_msg` (
+  `id` varchar(64) NOT NULL,
+  `send_user_id` varchar(64) NOT NULL,
+  `accept_user_id` varchar(64) NOT NULL,
+  `msg` varchar(255) NOT NULL,
+  `sign_flag` int(11) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat_msg`
+--
+
+LOCK TABLES `chat_msg` WRITE;
+/*!40000 ALTER TABLE `chat_msg` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat_msg` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `friends_request`
+--
+
+DROP TABLE IF EXISTS `friends_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friends_request` (
+  `id` varchar(64) NOT NULL,
+  `send_user_id` varchar(64) NOT NULL,
+  `accept_user_id` varchar(64) NOT NULL,
+  `request_date_time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `friends_request`
+--
+
+LOCK TABLES `friends_request` WRITE;
+/*!40000 ALTER TABLE `friends_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `friends_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `my_friends`
+--
+
+DROP TABLE IF EXISTS `my_friends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `my_friends` (
+  `id` varchar(64) NOT NULL,
+  `my_user_id` varchar(64) NOT NULL,
+  `my_friend_user_id` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `my_friends`
+--
+
+LOCK TABLES `my_friends` WRITE;
+/*!40000 ALTER TABLE `my_friends` DISABLE KEYS */;
+/*!40000 ALTER TABLE `my_friends` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` varchar(64) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `face_image` varchar(255) NOT NULL,
+  `face_image_big` varchar(255) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `qrcode` varchar(255) NOT NULL,
+  `cid` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'zhuxin-dev'
+--
+
+--
+-- Dumping routines for database 'zhuxin-dev'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-03 14:37:09
